645a955e5ed592c567517b580f6e288e07acd6ef
steveandcow <stephencao2002@gmail.com>
2023-02-20 15:40:12 -0500

    graphql setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphqlErrors, networkError}) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`GraphQL Error ${message}`);
+    });
+  }
+})
+
+const link = from([
+  errorLink,
+  new HttpLink({uri: "https://api.hackthenorth.com/v3/graphql"})
+])
+
+const client = new ApolloClient({
+  cache: new InMemoryCache,
+  link: link
+})
 
 function App() {
-  return (
+  return <ApolloProvider client={client}>(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +39,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )</ApolloProvider>;
 }
 
 export default App;
